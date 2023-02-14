670918fec09e91437897d9daec24b0b5526538d2
Jessica Groblewski <121062693+jessicagroblewski@users.noreply.github.com>
2023-02-13 19:40:31 -0500

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,33 +2,19 @@
   <h1>{{ msg }}</h1>
 
 <!-- My custom code -->
-<button class="outsideBtn" v-on:click="duplicate">Duplicate</button>
-<button class="outsideBtn" v-on:click="background">Background</button>
-<button class="outsideBtn" v-on:click="heading">Heading</button>
-<button class="outsideBtn" v-on:click="deleter">Delete</button>
-
-<div class="card" v-on:mouseover="hoover">
-  <h1 class="title">Chad of Cyber IST</h1>
-
-  <img class="giaImg" src="https://media.discordapp.net/attachments/963095262363017246/1020131830323744788/unknown.png?width=468&height=468" alt="Professor Giacobe">
+<div class="blocks">
+  <button id="topbutton" class="button">add new</button>
+  <button id="title" class="button">change new title</button>
+  <button id="bg" class="button">change bg color</button>
+  <button id="delete" class="button">delete last</button>
   
-<div class="textbox">
-  <details>
-    <summary class="haxbtn">Details</summary>
-    <p class="description pScale">Professor Giacobe may look like a simple man but perceptions can be misleading. Underneath the facade of a simple college of IST professor lies the holiness of an <strong>IST GOD</strong>!</p>
-  </details>
-
-  </div>
 </div>
-
-
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Documentation
-    </a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
-  </p>
+<div id="card" class="card">
+  <h3 id="heading" class="heading">Landscaping Buisness!</h3>
+  <img class="img" src="https://upload.wikimedia.org/wikipedia/commons/e/eb/Ash_Tree_-_geograph.org.uk_-_590710.jpg">
+  <p>This is a green tree! We love trees and want to cut them for you :) </p>
+  <button id="details" class="buttons">details</button>
+</div>
 
   <button type="button" @click="state.count++">count is: {{ state.count }}</button>
   <p>
